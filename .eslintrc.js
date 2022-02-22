ab0f71c871ccf0230477ff360978cacd818ff12c
jefftarira <jatt6076@gmail.com>
2022-02-22 15:39:41 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'all',
+        singleQuote: true,
+        trailingComma: 'es5',
+        semi: true,
+        tabWidth: 2,
+        arrowParens: 'always',
+        bracketSpacing: true,
+      },
+    ],
+    'no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$',
+      },
+    ],
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+    'react/self-closing-comp': 'warn',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
+};
